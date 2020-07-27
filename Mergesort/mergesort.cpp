97e5440bc19f28f0c3e47cfe758b0dd4bea88401
PhillipAColes <phillipanthonycoles@gmail.com>
2020-07-27 01:11:02 +0100

    spoke to soon... there was bug. Now fixed.

--- Mergesort/mergesort.cpp
+++ Mergesort/mergesort.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+static int inversions = 0;
+
 template<typename T>
 std::vector<T> slice(std::vector<T> const &v, int m, int n)
 {
@@ -29,13 +31,15 @@ std::vector<int> merge(std::vector<int> left,std::vector<int> right){
         // if we reach the end of left, then just return right
         if(i==left.size()){
             for(int a=j;a<right.size();a++)
-                merged.push_back(right[j]);
+                merged.push_back(right[a]);
             break;
         }
         // if we reach the end of right, then just return left
         if(j==right.size()){
-            for(int b=i;b<left.size();b++)
-                merged.push_back(left[i]);
+            for(int b=i;b<left.size();b++){
+                merged.push_back(left[b]);
+                inversions++;
+            }
             break;
         }
         // walk through the sorted left and right arrays, travelling from 
@@ -45,6 +49,8 @@ std::vector<int> merge(std::vector<int> left,std::vector<int> right){
             i++;
         }else{
             merged.push_back(right[j]);
+            // counting the number of inversions in our array
+            inversions++;
             j++;
         }
         k++;
@@ -79,11 +85,21 @@ std::vector<int> sort(std::vector<int> vec){
 int main(){
    
     std::vector<int> vec = {1,7,7,3,1,4,2,0,3,6,5,7,8,32,5,4,6,6};
+    //std::vector<int> vec = {1,3,5,2,4,6};
     
     std::vector<int> v_sorted = sort(vec);
     
+    std::cout << "unsorted array: ";
+    for(auto i : vec)
+        std::cout << i << " ";
+    std::cout << std::endl;
+    
+    std::cout << "sorted array: ";
     for(auto i : v_sorted)
         std::cout << i << " ";
+    std::cout << std::endl;
+    
+    std::cout << "array had " << inversions << " inversions" << endl;
     
     return 0;
     
